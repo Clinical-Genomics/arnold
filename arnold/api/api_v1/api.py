a21fc2958ee31fb413a22b20fa5031654c99ba94
mayabrandi <maya.brandi@scilifelab.se>
2021-09-16 15:35:10 +0200

    init beanie

--- arnold/api/api_v1/api.py
+++ arnold/api/api_v1/api.py
@@ -2,9 +2,26 @@ from fastapi import FastAPI
 
 from arnold.api.api_v1.endpoints import sample
 from arnold.api.api_v1.endpoints import prep
-
+from arnold.models.database import Sample, Prep
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from arnold.settings import settings
 
 app = FastAPI()
 
-app.include_router(sample.router, prefix="/api/v1", tags=["sample"])
-app.include_router(prep.router, prefix="/api/v1", tags=["prep"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to arnold"}
+
+
+@app.on_event("startup")
+async def main():
+    # Create Motor client
+    client = AsyncIOMotorClient(settings.db_uri)
+
+    # Init Beanie
+    await init_beanie(client[settings.db_name], document_models=[Sample, Prep])
+
+    app.include_router(sample.router, prefix="/api/v1", tags=["sample"])
+    app.include_router(prep.router, prefix="/api/v1", tags=["prep"])
--- arnold/models/database/prep/sars_cov_2_prep.py
+++ arnold/models/database/prep/sars_cov_2_prep.py
@@ -4,28 +4,28 @@ from pydantic import Field, BaseModel
 
 
 class SarsCov2Prep(BaseModel):
-    sample_concentration: Optional[float] = Field(None, alias="Concentration")
-    sample_size: Optional[float] = Field(None, alias="Peak Size")
-    lot_nr_tagmentation_beads: str = Field(..., alias="Tagmentation beads")
-    lot_nr__stop_tagment_buffer: str = Field(..., alias="Stop Tagment Buffer")
-    lot_nr_index: str = Field(..., alias="Index")
-    lot_nr_pcr_mix: str = Field(..., alias="PCR-mix")
-    lot_nr_tagmentation_wash_buffer: str = Field(..., alias="Tagmentation Wash Buffer")
-    lot_nr_h2o_library_preparation: str = Field(..., alias="Nuclease-free water")
-    lot_nr_TB1: str = Field(..., alias="TB1 HT")
-    pcr_instrument_tagmentation: str = Field(..., alias="PCR machine: Tagmentation")
-    pcr_instrument_amplification: str = Field(..., alias="PCR machine: Amplification")
-    library_preparation_method: str = Field(..., alias="Method document")
-    liquid_handling_system: str = Field(..., alias="Instrument")
-    pooling_method: str = Field(..., alias="Method document (pooling)")
-    clean_up_method: str = Field(..., alias="Method document (Clean-up)")
-    lot_nr_beads_clean_up: str = Field(..., alias="Purification beads")
-    lot_nr_etoh_clean_up: str = Field(..., alias="Ethanol")
-    lot_nr_h2o_clean_up: str = Field(..., alias="Nuclease-free water")
-    lot_nr_resuspension_buffer_clean_up: str = Field(..., alias="Resuspension buffer")
-    finished_library_concentration: float = Field(..., alias="Concentration")
-    finished_library_concentration_nm: float = Field(..., alias="Concentration (nM)")
-    finished_library_size: Optional[float] = Field(None, alias="Size (bp)")
+    sample_concentration: Optional[float]
+    sample_size: Optional[float]
+    lot_nr_tagmentation_beads: str
+    lot_nr__stop_tagment_buffer: str
+    lot_nr_index: str
+    lot_nr_pcr_mix: str
+    lot_nr_tagmentation_wash_buffer: str
+    lot_nr_h2o_library_preparation: str
+    lot_nr_TB1: str
+    pcr_instrument_tagmentation: str
+    pcr_instrument_amplification: str
+    library_preparation_method: str
+    liquid_handling_system: str
+    pooling_method: str
+    clean_up_method: str
+    lot_nr_beads_clean_up: str
+    lot_nr_etoh_clean_up: str
+    lot_nr_h2o_clean_up: str
+    lot_nr_resuspension_buffer_clean_up: str
+    finished_library_concentration: float
+    finished_library_concentration_nm: float
+    finished_library_size: Optional[float]
 
     class Config:
         allow_population_by_field_name = True
