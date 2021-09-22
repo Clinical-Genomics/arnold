0db7b3f42ca4f769dfd68d489c32c0d44eab0d96
mayabrandi <maya.brandi@scilifelab.se>
2021-09-22 15:59:29 +0200

    optinal fields (#2)

--- arnold/models/database/prep/sars_cov_2_prep.py
+++ arnold/models/database/prep/sars_cov_2_prep.py
@@ -1,30 +1,29 @@
 from typing import Optional
-
-from pydantic import Field, BaseModel
+from pydantic import BaseModel
 
 
 class SarsCov2Prep(BaseModel):
     sample_concentration: Optional[float]
     sample_size: Optional[float]
-    lot_nr_tagmentation_beads: str
-    lot_nr__stop_tagment_buffer: str
-    lot_nr_index: str
-    lot_nr_pcr_mix: str
-    lot_nr_tagmentation_wash_buffer: str
-    lot_nr_h2o_library_preparation: str
-    lot_nr_TB1: str
-    pcr_instrument_tagmentation: str
-    pcr_instrument_amplification: str
-    library_preparation_method: str
-    liquid_handling_system: str
-    pooling_method: str
-    clean_up_method: str
-    lot_nr_beads_clean_up: str
-    lot_nr_etoh_clean_up: str
-    lot_nr_h2o_clean_up: str
-    lot_nr_resuspension_buffer_clean_up: str
-    finished_library_concentration: float
-    finished_library_concentration_nm: float
+    lot_nr_tagmentation_beads: Optional[str]
+    lot_nr__stop_tagment_buffer: Optional[str]
+    lot_nr_index: Optional[str]
+    lot_nr_pcr_mix: Optional[str]
+    lot_nr_tagmentation_wash_buffer: Optional[str]
+    lot_nr_h2o_library_preparation: Optional[str]
+    lot_nr_TB1: Optional[str]
+    pcr_instrument_tagmentation: Optional[str]
+    pcr_instrument_amplification: Optional[str]
+    library_preparation_method: Optional[str]
+    liquid_handling_system: Optional[str]
+    pooling_method: Optional[str]
+    clean_up_method: Optional[str]
+    lot_nr_beads_clean_up: Optional[str]
+    lot_nr_etoh_clean_up: Optional[str]
+    lot_nr_h2o_clean_up: Optional[str]
+    lot_nr_resuspension_buffer_clean_up: Optional[str]
+    finished_library_concentration: Optional[float]
+    finished_library_concentration_nm: Optional[float]
     finished_library_size: Optional[float]
 
     class Config:
