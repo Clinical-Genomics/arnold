d4b9b2bcc0eb34b5ae4bbd07cefbb9a20a97c879
mayabrandi <maya.brandi@scilifelab.se>
2021-10-09 07:40:54 +0200

    adding lims step name

--- arnold/models/database/step.py
+++ arnold/models/database/step.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from pydantic import Field, validator, BaseModel
+from typing import Optional, Dict
+from pydantic import Field, BaseModel
 
 from pydantic import Extra
 
@@ -8,14 +8,18 @@ class Step(BaseModel):
     """Artifact related fields that are not udfs."""
 
     id: str = Field(..., alias="_id")
+    step_id: str
     prep_id: str
     step_type: str
     sample_id: str
     workflow: str
+    lims_step_name: Optional[str]
     well_position: Optional[str]
     container_name: Optional[str]
     index_name: Optional[str]
     nr_samples: Optional[int]
+    artifact_udfs: Optional[Dict[str]]
+    process_udfs: Optional[Dict[str]]
 
     class Config:
         allow_population_by_field_name = True
