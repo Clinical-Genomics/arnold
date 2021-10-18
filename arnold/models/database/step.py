8cba0bff4c095fc9f25c00a9fa3b903fe29b9e3c
mayabrandi <maya.brandi@scilifelab.se>
2021-10-18 16:05:26 +0200

    change order

--- arnold/models/database/step.py
+++ arnold/models/database/step.py
@@ -8,23 +8,23 @@ from pydantic import Extra
 class Step(BaseModel):
     """Artifact related fields that are not udfs."""
 
-    id: str = Field(..., alias="_id")
-    step_id: str
     prep_id: str
     step_type: str
     sample_id: str
     workflow: str
     lims_step_name: Optional[str]
+    id: str = Field(..., alias="_id")
+    step_id: str
     well_position: Optional[str]
-    container_type: Optional[str]
+    artifact_name: Optional[str]
     container_name: Optional[str]
     container_id: Optional[str]
+    container_type: Optional[str]
     index_name: Optional[str]
     nr_samples_in_pool: Optional[int]
-    artifact_name: Optional[str]
+    date_run: Optional[datetime]
     artifact_udfs: Optional[dict]
     process_udfs: Optional[dict]
-    date_run: Optional[datetime]
 
     class Config:
         allow_population_by_field_name = True
