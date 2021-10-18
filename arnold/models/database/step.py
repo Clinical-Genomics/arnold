421d4e12d1dd425b95087d697f6b2f176b290a71
mayabrandi <maya.brandi@scilifelab.se>
2021-10-18 15:58:07 +0200

    date field

--- arnold/models/database/step.py
+++ arnold/models/database/step.py
@@ -1,5 +1,6 @@
-from typing import Optional, Dict
+from typing import Optional
 from pydantic import Field, BaseModel
+from datetime import datetime
 
 from pydantic import Extra
 
@@ -15,11 +16,15 @@ class Step(BaseModel):
     workflow: str
     lims_step_name: Optional[str]
     well_position: Optional[str]
+    container_type: Optional[str]
     container_name: Optional[str]
+    container_id: Optional[str]
     index_name: Optional[str]
-    nr_samples: Optional[int]
+    nr_samples_in_pool: Optional[int]
+    artifact_name: Optional[str]
     artifact_udfs: Optional[dict]
     process_udfs: Optional[dict]
+    date_run: Optional[datetime]
 
     class Config:
         allow_population_by_field_name = True
