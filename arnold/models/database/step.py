37876f51af13f9beffab4360506d35d6fe545ea7
mayabrandi <maya.brandi@scilifelab.se>
2021-10-18 17:05:36 +0200

    Step model (#10)(patch)
    ### Changed
    - changed the step model. Adding some fields.

--- arnold/models/database/step.py
+++ arnold/models/database/step.py
@@ -1,5 +1,6 @@
-from typing import Optional, Dict
+from typing import Optional
 from pydantic import Field, BaseModel
+from datetime import datetime
 
 from pydantic import Extra
 
@@ -7,17 +8,21 @@ from pydantic import Extra
 class Step(BaseModel):
     """Artifact related fields that are not udfs."""
 
-    id: str = Field(..., alias="_id")
-    step_id: str
     prep_id: str
     step_type: str
     sample_id: str
     workflow: str
     lims_step_name: Optional[str]
+    id: str = Field(..., alias="_id")
+    step_id: str
     well_position: Optional[str]
+    artifact_name: Optional[str]
     container_name: Optional[str]
+    container_id: Optional[str]
+    container_type: Optional[str]
     index_name: Optional[str]
-    nr_samples: Optional[int]
+    nr_samples_in_pool: Optional[int]
+    date_run: Optional[datetime]
     artifact_udfs: Optional[dict]
     process_udfs: Optional[dict]
 
