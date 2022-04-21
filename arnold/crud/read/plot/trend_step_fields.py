07f09e5da5ac0063d09e86ae8eadd5a42d47aa21
mayabrandi <maya.brandi@scilifelab.se>
2022-04-21 11:48:42 +0200

    field type udf

--- arnold/crud/read/plot/trend_step_fields.py
+++ arnold/crud/read/plot/trend_step_fields.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Literal
 
 from arnold.adapter import ArnoldAdapter
 from arnold.crud.read.plot.format_plot_data import format_grouped_plot_data, format_plot_data
@@ -11,6 +11,7 @@ def trend_step_fields(
     step_type: str,
     field: str,
     group: Optional[str],
+    udf_type: Literal["artifact_udfs", "process_udfs"],
 ) -> list:
 
     lookup = {
@@ -25,7 +26,7 @@ def trend_step_fields(
     match = {
         "$match": {
             "date_run": {"$exists": "True"},
-            field: {"$exists": "True"},
+            f"{udf_type}.{field}": {"$exists": "True"},
             "workflow": workflow,
             "step_type": step_type,
         }
@@ -34,20 +35,17 @@ def trend_step_fields(
         "$project": {
             "month": {"$month": "$date_run"},
             "year": {"$year": "$date_run"},
-            field: 1,
+            f"{udf_type}.{field}": 1,
         }
     }
     match_year = {"$match": {"year": year}}
-    field_replaced_dot = field.split(".")[1]
     group_by = {
         "$group": {
             "_id": {"month": "$month"},
-            field_replaced_dot: {"$push": f"${field}"},
+            field: {"$push": f"${field}"},
         }
     }
-    add_average = {
-        "$addFields": {f"average_{field_replaced_dot}": {"$avg": f"${field_replaced_dot}"}}
-    }
+    add_average = {"$addFields": {f"average_{field}": {"$avg": f"${field}"}}}
 
     if group:
         match["$match"][f"sample.{group}"] = {"$exists": "True"}
@@ -57,10 +55,9 @@ def trend_step_fields(
     pipe = [lookup, unwind, match, project, match_year, group_by, add_average]
 
     data = list(adapter.step_collection.aggregate(pipe))
+    print(pipe)
     return (
-        format_grouped_plot_data(
-            plot_data=data, group_field=group, trend_field=f"average_{field_replaced_dot}"
-        )
+        format_grouped_plot_data(plot_data=data, group_field=group, trend_field=f"average_{field}")
         if group
-        else format_plot_data(plot_data=data, trend_field=f"average_{field_replaced_dot}")
+        else format_plot_data(plot_data=data, trend_field=f"average_{field}")
     )
