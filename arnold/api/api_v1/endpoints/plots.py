46fb0287c0aec0f05029a478963aafc736b58f62
Mropat <mariaropat@gmail.com>
2022-06-28 10:55:06 +0200

    query formatting

--- arnold/api/api_v1/endpoints/plots.py
+++ arnold/api/api_v1/endpoints/plots.py
@@ -22,34 +22,32 @@ def received(
     date_max: Optional[dt.date] = dt.date.max,
 ):
     samples = adapter.sample_collection.find(
-        [
-            {
-                "$match": {
-                    "delivery_date": {"$exists": 1},
-                    "received_date": {"$gte": date_min.isoformat(), "$lte": date_max.isoformat()},
-                }
-            },
-            {
-                "$project": {
-                    "received_date": 1,
-                    "sample_id": 1,
-                    "tag": {"$substr": ["$application", 0, 3]},
-                    "received_year": {"$year": "$received_date"},
-                    "received_month": {"$month": "$received_date"},
-                }
-            },
-            {
-                "$group": {
-                    "_id": {
-                        "year": "$received_year",
-                        "month": "$received_month",
-                        "tag": "$tag",
-                    },
-                    "count": {"$sum": 1},
-                }
-            },
-            {"$sort": {"_id": -1}},
-        ]
+        {
+            "$match": {
+                "delivery_date": {"$exists": 1},
+                "received_date": {"$gte": date_min.isoformat(), "$lte": date_max.isoformat()},
+            }
+        },
+        {
+            "$project": {
+                "received_date": 1,
+                "sample_id": 1,
+                "tag": {"$substr": ["$application", 0, 3]},
+                "received_year": {"$year": "$received_date"},
+                "received_month": {"$month": "$received_date"},
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "year": "$received_year",
+                    "month": "$received_month",
+                    "tag": "$tag",
+                },
+                "count": {"$sum": 1},
+            }
+        },
+        {"$sort": {"_id": -1}},
     )
 
     return list(samples)
