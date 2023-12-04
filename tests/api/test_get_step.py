2acc527d235d6b1c17437910a8e0597878b8fd39
Christian Oertlin <c.oertlin@gmail.com>
2023-12-04 13:33:12 +0100

    fix broken test

--- tests/api/test_get_step.py
+++ tests/api/test_get_step.py
@@ -7,7 +7,7 @@ client = TestClient(router)
 
 def test_get_step(mocker, fast_app_client, valid_step):
     # GIVEN valid step request data
-    mocker.patch("arnold.crud.read.step.find_step", return_value=valid_step)
+    mocker.patch("arnold.api.api_v1.endpoints.step.find_step", return_value=valid_step)
 
     # WHEN running the user request with the data
     response = fast_app_client.get("/api/v1/step/some_id")
@@ -16,12 +16,4 @@ def test_get_step(mocker, fast_app_client, valid_step):
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_get_step_failing(mocker, fast_app_client):
-    # GIVEN that the step is not in the database but the create_step fails
-    mocker.patch("arnold.crud.read.step.find_step", return_value=None)
 
-    # WHEN running the step request with data
-    response = fast_app_client.get("/api/v1/step/some_id")
-
-    # THEN assert status is 405
-    assert response.status_code == status.HTTP_200_OK
